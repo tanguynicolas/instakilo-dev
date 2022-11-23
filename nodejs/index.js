f9920b76aee0630de49058ca87564c05b520594e
tata kaba <diakitetatakaba50@gmail.com>
2022-11-23 09:51:58 +0100

    ajout

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', (socket) => {
+    console.log('connecté');
+   // socket.on('deconnecté', () => {
+       // console.log('deconnecté');
+   // });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+server.listen(3000, () => {
+  console.log('ecoute le port *:3000');
+});
